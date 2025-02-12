e9a1af670f73b0c95109aa365b3b554890813d95
UMARU-xiaomai <UMARU2017@163.com>
2025-02-12 23:31:16 +0800

    Initial Commit

---
+++ ScanCutter/ScanCutterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanCutterApp.swift
+//  ScanCutter
+//
+//  Created by ShimitsuKoi on 2025/2/12.
+//
+
+import SwiftUI
+
+@main
+struct ScanCutterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
